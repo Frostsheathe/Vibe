22edd000224bd16a4c0bd1959dd979e485682545
Frostsheathe <58339415+Frostsheathe@users.noreply.github.com>
2021-12-29 11:05:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let blem = "https://drive.google.com/file/d/1kIsupF-u6HOcqqzFcQhhdTISbL-CUESk/view?usp=sharing";
+let song2 = "https://drive.google.com/file/d/1WdDXtuzvGyR-WqXF-LYUvSENax_-A64Y/view?usp=sharing";
+let song3 = "https://drive.google.com/file/d/1vqgAIqT8IXr9azg2wC61NiPM40GKSRS-/view?usp=sharing";
+let song4 = "https://drive.google.com/file/d/1-AN4BX3FqKISYzzCgWov70YrwzpC-74m/view?usp=sharing";
+let song5 = "https://drive.google.com/file/d/1QbrT8BCbcSlmNHpYzJWRnv_XuXT-OJ_W/view?usp=sharing";
+let song6 = "https://drive.google.com/file/d/1CeaJW4NoDiDa_RSSdT-kZ74NwP5O7JnP/view?usp=sharing";
+id1 = "1kIsupF-u6HOcqqzFcQhhdTISbL-CUESk"
+url = `https://docs.google.com/uc?export=download&id={id}`
+    // Get the elements with class="column"
+document.querySelector("#audio").setAttribute("src", "blem");
